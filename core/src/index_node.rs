1673506f7020df7c40627eb881c950fd19337da5
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-03-18 09:17:44 +0100

    Created methods to asynchronously receive separate connections from either a consumer or relay node

--- core/src/index_node.rs
+++ core/src/index_node.rs
@@ -1,21 +1,39 @@
-struct IndexNode {
+use async_std::{
+    task,
+    net::{IpAddr, SocketAddr, TcpListener, Incoming}
+};
+
+pub struct IndexNode {
     ip: IpAddr,
-    available_relays: Vec<_>, // Incomplete, need relay node struct or some other addressable tuple
+    available_relays: Vec<_> // Incomplete, blocked by relay node struct or some other addressable tuple
 }
 
 impl IndexNode {
     pub fn new(ip: IpAddr) -> IndexNode {
         IndexNode {
-            ip = ip,
+            ip: ip,
             available_relays: Vec::new()
         }
     }
 
-    fn listen_for_relay(&self) {
-
+    async fn listen_for_relay(&self, port: u16) {
+        self.listen(port, |connection| async {
+            // handle relay registration
+        }).await
     }
-
-    fn listen_for_consumer(&self) {
-
+    
+    async fn listen_for_consumer(&self, port: u16) {
+        self.listen(port, |connection| async {
+            // handle consumer request
+        }).await
+    }
+    
+    async fn listen(&self, port: u16, connection_handler: dyn Fn(Incoming)) {
+        let socket = SocketAddr::new(self.ip, port);
+        let listener = TcpListener::bind(socket).await;
+    
+        for connection in listener.incoming() {
+            task::spawn(connection_handler(connection)).expect("Failed to receive connection")
+        }
     }
 }
