ed1024bff5b202b57d68f04ba3f8c23c8c401c87
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-03-18 10:49:17 +0100

    Fixed multiple 'hidden' errors centered arround borrowing and result types

--- core/src/index_node.rs
+++ core/src/index_node.rs
@@ -1,39 +1,47 @@
 use async_std::{
     task,
-    net::{IpAddr, SocketAddr, TcpListener, Incoming}
+    net::{IpAddr, SocketAddr, TcpListener, Incoming, TcpStream}
 };
+
+use std::future::Future;
+use async_std::prelude::*;
 
 pub struct IndexNode {
     ip: IpAddr,
-    available_relays: Vec<_> // Incomplete, blocked by relay node struct or some other addressable tuple
+    //available_relays: Vec<_> // BLOCKED: relay node struct or some other addressable tuple
 }
 
 impl IndexNode {
     pub fn new(ip: IpAddr) -> IndexNode {
         IndexNode {
             ip: ip,
-            available_relays: Vec::new()
+            //available_relays: Vec::new()
         }
     }
 
     async fn listen_for_relay(&self, port: u16) {
-        self.listen(port, |connection| async {
+        self.listen(port, |stream| async {
             // handle relay registration
+            // BLOCKED: ROnion protocol
         }).await
     }
     
     async fn listen_for_consumer(&self, port: u16) {
-        self.listen(port, |connection| async {
+        self.listen(port, |stream| async {
             // handle consumer request
+            // BLOCKED: ROnion protocol
         }).await
     }
     
-    async fn listen(&self, port: u16, connection_handler: dyn Fn(Incoming)) {
+    async fn listen<F>(&self, port: u16, connection_handler: impl Fn(TcpStream) -> F)
+        where F: Future + Send + 'static
+    {
         let socket = SocketAddr::new(self.ip, port);
-        let listener = TcpListener::bind(socket).await;
+        let listener = TcpListener::bind(socket).await.expect("Failed to bind to socket");
     
-        for connection in listener.incoming() {
-            task::spawn(connection_handler(connection)).expect("Failed to receive connection")
+        let mut incoming = listener.incoming();
+        while let Some(stream) = incoming.next().await {
+            task::spawn(connection_handler(stream.expect("Failed to receive connection")));
         }
     }
 }
