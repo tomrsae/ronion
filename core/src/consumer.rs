981df1f5295a414add5ec29da2ad2a4afcca9ef3
Magnus Steensland <mag.steensland@gmail.com>
2022-03-19 16:16:48 +0100

    Added send/recv methods

--- core/src/consumer.rs
+++ core/src/consumer.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 use aes::Aes256;
 use async_std::{
-    io::{Cursor, ReadExt, WriteExt},
+    io::{Cursor, WriteExt},
     net::TcpStream,
 };
 
@@ -16,20 +16,21 @@ pub struct Consumer {
     reader: OnionReader<TcpStream, Aes256>,
     writer: OnionWriter<TcpStream, Aes256>,
     onionizer: Onionizer,
-    entry_target: Target,
 }
 
 impl Consumer {
-    fn new(
-        n: usize,
-        stream: &TcpStream,
-        peer_public_keys: Vec<[u8; 32]>,
-        mut targets: Vec<Target>,
-    ) -> Self {
+    async fn new(n: usize, stream: &TcpStream, mut targets: Vec<Target>) -> Self {
         let index_pub_key: [u8; 32] = [
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
             25, 26, 27, 28, 29, 30, 31, 32,
         ];
+
+        let (mut index_reader, _) = Consumer::dial("", index_pub_key).await;
+
+        let index_onion = index_reader.read().await.unwrap();
+
+        let (target_ids, target_IPs, peer_public_keys) = Consumer::parse_index_onion(index_onion);
+
         let mut secrets = Secret::create_secrets(n, peer_public_keys);
         let mut public_keys = Vec::<[u8; 32]>::with_capacity(n);
         let mut reader: OnionReader<TcpStream, Aes256>;
@@ -40,14 +41,12 @@ impl Consumer {
             public_keys.push(secret.gen_pub_key().as_bytes().to_owned());
             ciphers.push(secret.gen_symmetric_cipher());
         }
-        let entry_target = targets.remove(n - 1);
 
         Consumer {
             public_keys,
             reader: OnionReader::new(stream.clone(), ciphers[n - 1].clone()),
             writer: OnionWriter::new(stream.clone(), ciphers[n - 1].clone()),
             onionizer: Onionizer::new(targets, ciphers.clone()),
-            entry_target,
         }
     }
 
@@ -81,23 +80,26 @@ impl Consumer {
         )
     }
 
-    // pub async fn handshake_entry(
-    //     stream: &mut TcpStream,
-    //     entry_target: Target,
-    //     entry_pub_key: [u8; 32],
-    // ) {
-    //     let secret = Secret::new(index_pub_key);
-    //     let cipher = secret.gen_symmetric_cipher();
-    // }
+    fn parse_index_onion(onion: Onion) -> (Vec<Target>, Vec<Target>, Vec<[u8; 32]>) {
+        let ids = Vec::<Target>::new();
+        let ips = Vec::<Target>::new();
+        let keys = Vec::<[u8; 32]>::new();
 
-    //Fix return
-    pub fn send_message() -> bool {
-        true
+        //onion.payload.chunks_exact(chunk_size).map(f)....
+        panic!("data format not yet implemented");
+
+        (ids, ips, keys)
     }
 
-    //Fix return
-    pub fn recv_message() -> bool {
-        true
+    pub async fn send_message(&mut self, payload: Vec<u8>) -> () {
+        let onion = self.onionizer.grow_onion(payload).await;
+        self.writer.write(onion).await.unwrap();
+    }
+
+    pub async fn recv_message(&mut self) -> Vec<u8> {
+        let onion = self.reader.read().await.unwrap();
+        //Check target?? (probably unneccesary)
+        onion.payload
     }
 }
 
@@ -120,19 +122,19 @@ impl Onionizer {
         writer.into_inner()
     }
 
-    pub async fn grow_onion(&self, num_layers: usize, payload: Vec<u8>) -> Vec<u8> {
-        if num_layers <= 0 {
-            panic!("Cannot layer onion with 'nothing'")
-        }
+    pub async fn grow_onion(&self, payload: Vec<u8>) -> Onion {
         let targets = self.targets.clone();
         let ciphers = self.ciphers.clone();
         let mut onion_load =
             Onionizer::onionize(targets[0].clone(), payload, ciphers[0].clone()).await;
 
-        for i in 1..num_layers {
+        for i in 1..self.targets.len() - 2 {
             onion_load =
                 Onionizer::onionize(targets[i].clone(), onion_load, ciphers[i].clone()).await
         }
-        onion_load
+        Onion {
+            target: targets[targets.len() - 1].clone(),
+            payload: onion_load,
+        }
     }
 }
