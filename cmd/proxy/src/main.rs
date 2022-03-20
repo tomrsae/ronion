0ccde939345317627ddcaf11f9dd88207272560a
Magnus Steensland <mag.steensland@gmail.com>
2022-03-20 13:09:37 +0100

    Proxy demo initial

---
+++ cmd/proxy/src/main.rs
@@ -0,0 +1,7 @@
+mod proxy;
+
+fn main() {
+    print!("Monke");
+    // proxy::Proxy::listen(context, svr_cfg); //Listen for relays
+    // proxy::Proxy::listen(context, svr_cfg); //Listen for consumers
+}
