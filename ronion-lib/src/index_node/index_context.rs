0730abcfbae41ba4e47d77516414d05dfa1a8b4f
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-03-23 15:41:35 +0100

    Finished implementing changes made to crypto

--- ronion-lib/src/index_node/index_context.rs
+++ ronion-lib/src/index_node/index_context.rs
@@ -1,11 +1,12 @@
-use crate::protocol::onion::Relay;
+use crate::{protocol::onion::Relay, crypto::{ServerCrypto}};
 
 use super::uid_generator::UIDGenerator;
 
 pub struct IndexContext {
     pub available_relays: Vec<Relay>,
     pub circ_id_generator: UIDGenerator,
-    pub relay_id_generator: UIDGenerator
+    pub relay_id_generator: UIDGenerator,
+    pub crypto: ServerCrypto
 }
 
 impl IndexContext {
@@ -13,7 +14,8 @@ impl IndexContext {
         IndexContext {
             available_relays: Vec::new(),
             circ_id_generator: UIDGenerator::new(10),
-            relay_id_generator: UIDGenerator::new(10)
+            relay_id_generator: UIDGenerator::new(10),
+            crypto: ServerCrypto::new()
         }
     }
 }
