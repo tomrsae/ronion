f075c6e3b5c9b2e5d9726fc474a61bc571a49298
Magnus Steensland <mag.steensland@gmail.com>
2022-03-23 20:55:21 +0100

    Commented out unfinished code to allow compilation

--- ronion-lib/src/relay_node/relay_context.rs
+++ ronion-lib/src/relay_node/relay_context.rs
@@ -3,17 +3,17 @@ use async_std::sync::Arc;
 use super::circuit::Circuit;
 
 pub struct RelayContext {
-    pub circuits: Vec<Arc<Circuit>>
+    pub circuits: Vec<Arc<Circuit>>,
 }
 
 impl RelayContext {
     pub fn new() -> Self {
         Self {
-            circuits: Vec::new()
+            circuits: Vec::new(),
         }
     }
 
-    pub fn get_circuit(&self, circuit_id: u32) -> Option<Arc<Circuit>> {
-        self.circuits.iter().find(|circuit| circuit.id == circuit_id).map(|circuit| *circuit)
-    }
+    // pub fn get_circuit(&self, circuit_id: u32) -> Option<Arc<Circuit>> {
+    //     self.circuits.iter().find(|circuit| circuit.id == circuit_id).map(|circuit| *circuit)
+    // }
 }
--- ronion-lib/src/relay_node/relay_node.rs
+++ ronion-lib/src/relay_node/relay_node.rs
@@ -1,24 +1,22 @@
 use async_std::{
-    task,
-    sync::{Arc, Mutex},
-    prelude::*,
+    io::{Error, ErrorKind, Result},
     net::{IpAddr, SocketAddr, TcpListener, TcpStream},
-    io::{Result, Error, ErrorKind}
+    prelude::*,
+    sync::{Arc, Mutex},
+    task,
 };
 
-use crate::{
-    protocol::{
-        io::{RawOnionWriter, RawOnionReader},
-        onion::{Onion, Target, Message, Relay}
-    }
+use crate::protocol::{
+    io::{RawOnionReader, RawOnionWriter},
+    onion::{Message, Onion, Relay, Target},
 };
 
-use super::{relay_context::RelayContext, circuit_connection::CircuitConnection, circuit::Circuit};
+use super::{circuit::Circuit, circuit_connection::CircuitConnection, relay_context::RelayContext};
 
 pub struct RelayNode {
     ip: IpAddr,
     port: u16,
-    context: Arc<Mutex<RelayContext>>
+    context: Arc<Mutex<RelayContext>>,
 }
 
 impl RelayNode {
@@ -26,45 +24,54 @@ impl RelayNode {
         Self {
             ip: ip,
             port: port,
-            context: Arc::new(Mutex::new(RelayContext::new()))
+            context: Arc::new(Mutex::new(RelayContext::new())),
         }
     }
 
     pub fn start(&self) {
         let socket = SocketAddr::new(self.ip, self.port);
         let listen_future = self.listen(socket);
-        
+
         task::block_on(listen_future); // bytte til async?
     }
 
     async fn listen(&self, socket: SocketAddr) {
-        let listener = TcpListener::bind(socket).await.expect("Failed to bind to socket");
-        
+        let listener = TcpListener::bind(socket)
+            .await
+            .expect("Failed to bind to socket");
+
         let mut incoming = listener.incoming();
         while let Some(stream) = incoming.next().await {
             let context = self.context.clone();
-            let handler_future
-                = async {
-                    Self::handle_connection(stream.expect("Failed to read from stream"), context)
+            let handler_future = async {
+                Self::handle_connection(stream.expect("Failed to read from stream"), context)
                     .await
                     .expect("Failed to handle connection")
-                };
-            
+            };
+
             task::spawn(handler_future);
         }
     }
 
-    async fn handle_connection(incoming_stream: TcpStream, context: Arc<Mutex<RelayContext>>) -> Result<()> {
-        let incoming = CircuitConnection { stream: incoming_stream };
+    async fn handle_connection(
+        incoming_stream: TcpStream,
+        context: Arc<Mutex<RelayContext>>,
+    ) -> Result<()> {
+        let incoming = CircuitConnection {
+            stream: incoming_stream,
+        };
 
-        if let Ok(outgoing_stream) = TcpStream::connect("localhost:7070").await { // fix
+        if let Ok(outgoing_stream) = TcpStream::connect("localhost:7070").await {
+            // fix
             let circuit = Arc::new(Circuit {
                 id: 2_u32, //idk
-                outgoing: CircuitConnection { stream: outgoing_stream },
-                incoming: incoming
+                outgoing: CircuitConnection {
+                    stream: outgoing_stream,
+                },
+                incoming: incoming,
             });
 
-            task::spawn(circuit.activate());
+            //task::spawn(circuit.activate());
 
             let mut guard = context.lock().await;
             let context_locked = &mut *guard;
@@ -76,6 +83,4 @@ impl RelayNode {
 
         Ok(())
     }
-
-
 }
