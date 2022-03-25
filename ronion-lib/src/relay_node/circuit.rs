42992ab0b39baeed867aee79bc7d0ab8e20a1e17
Henduww <tommy.saetre@hotmail.com>
2022-03-25 13:35:32 +0100

    Removed unused code from circuit

--- ronion-lib/src/relay_node/circuit.rs
+++ ronion-lib/src/relay_node/circuit.rs
@@ -1,9 +1,4 @@
 use aes::Aes256;
-use async_std::{io::{Result, Read}, net::TcpStream};
-
-use crate::{crypto::ServerSecret, protocol::onion::Onion};
-
-use super::circuit_connection::CircuitConnection;
 
 #[derive(Clone)]
 pub struct Circuit {
@@ -14,15 +9,6 @@ pub struct Circuit {
 }
 
 impl Circuit {
-    pub fn new(id: u32, secret: ServerSecret, peer_key: [u8; 32]) -> Self {
-        Self {
-            id: id,
-            peer_key: peer_key,
-            public_key: secret.public_key(),
-            symmetric_cipher: secret.symmetric_cipher(peer_key)
-        }
-    }
-
     pub fn symmetric_cipher(&self) -> Aes256 {
         self.symmetric_cipher.clone()
     }
