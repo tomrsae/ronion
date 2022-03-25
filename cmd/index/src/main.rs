05f762ef1f16e2015c137ac0cae5b7f760f4b20f
Magnus Steensland <mag.steensland@gmail.com>
2022-03-25 16:21:57 +0100

    Barebones index program cli

--- cmd/index/src/main.rs
+++ cmd/index/src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, net::IpAddr};
+
+use core::index_node::index_node::IndexNode;
+
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+
+    let (ip, port) = parse_arguments(args);
+    let node = IndexNode::new(ip, port);
+
+    node.start();
+}
+
+fn parse_arguments(args: Vec<String>) -> (IpAddr, u16) {
+    let addr: IpAddr = args[1].parse().unwrap();
+    let port: u16 = args[2].parse().unwrap();
+
+    (addr, port)
 }
